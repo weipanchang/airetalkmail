a729e669f2e172e11900c9e65659b210cc4e79ac
weipanchang <weipanchang@hotmail.com>
2015-01-29 20:50:54 -0800

    generate country list

--- readweb.py
+++ readweb.py
@@ -4,15 +4,15 @@ import urllib2
 import json
 import MySQLdb
 
-ip='42.121.143.104'
-response = urllib2.urlopen('http://freegeoip.net/json/' + ip)
-#print 'RESPONSE:', response
-#print 'URL     :', response.geturl()
-data = response.read()
-print data
-loc=json.loads(data)
-print loc['country_name']
-
+#ip='42.121.143.104'
+#response = urllib2.urlopen('http://freegeoip.net/json/' + ip)
+##print 'RESPONSE:', response
+##print 'URL     :', response.geturl()
+#data = response.read()
+#print data
+#loc=json.loads(data)
+#print loc['country_name']
+country=[]
 
 db = MySQLdb.connect(host="localhost", # your host, usually localhost
                      user="root", # your username
@@ -22,4 +22,14 @@ db = MySQLdb.connect(host="localhost", # your host, usually localhost
 cur = db.cursor() 
 cur.execute("SELECT * FROM member limit 20" )
 for row in cur.fetchall() :
-    print row[0]
+    #print row[0], row[1], " ", row[2], " ",row[3], " ",row[4], " ",row[5], " ",row[6], " ",row[7] ,row[8], " ",row[9],row[10], " ",row[11], "\n"
+    #print row[3], " ", row[5]," ",row[11], "\n"
+    ip =row[11]
+    response = urllib2.urlopen('http://freegeoip.net/json/' + ip)
+    data = response.read()
+    loc=json.loads(data)
+    if loc['country_name'].encode('ascii', 'ignore') not in country:
+        country.append(loc['country_name'].encode('ascii', 'ignore'))
+    
+print country
+    
